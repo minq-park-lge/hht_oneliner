bbe038292432b6e93b67629c4f791443ab5601c8
Minkyu Park <minq.park@lge.com>
2026-05-25 17:37:21 +0900

    doorlock: update 잘되는 ht 깃

---
+++ door-lock/git_clone_from_ht.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone git@github.com:HT-IoT-Partner/tmcdd15r_v6.git --recursive
--- door-lock/git_clone_from_minq.sh
+++ door-lock/git_clone_from_minq.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clone git@github.com:minq-park-lge/tmcdd15r_v6.git --recursive -b topic/conn/compileok
+git clone git@github.com:minq-park-lge/tmcdd15r_v6.git tmcdd15r_v6_lg --recursive -b topic/conn/compileok
