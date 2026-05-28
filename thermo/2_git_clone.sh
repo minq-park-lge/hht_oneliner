c689670d2b206b85ea1f021decfab9854eebf378
Minkyu Park <minq.park@lge.com>
2026-05-29 04:46:17 +0900

    fix: add source, ...etc

--- thermo/2_git_clone.sh
+++ thermo/2_git_clone.sh
@@ -3,7 +3,7 @@ TOP_DIR=$(pwd)
 git clone https://github.com/NXP/matter.git -b release/v1.6.0-TE2 nxp_matter
 
 cd nxp_matter
-./scripts/checkout_submodules.py --shallow --platform nxp --recursive
+python3 ./scripts/checkout_submodules.py --shallow --platform nxp --recursive
 source scripts/bootstrap.sh -p nxp
 pip install pyyaml
 third_party/nxp/nxp_matter_support/scripts/update_nxp_sdk.py --platform common
@@ -12,7 +12,7 @@ export ARMGCC_DIR=$(pwd)/.environment/cipd/packages/arm
 
 
 #HHT code
-
+cd /examples/thermostat/nxp
 git clone git@github.com:HT-IoT-Partner/thermostat_nxp_rt1060_v1_6_TE2.git src
 cd src
-git am $TOP_DIR/0001-linux-build.patch
+git am 0001-linux-build.patch
