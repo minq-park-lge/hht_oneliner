05111919203164b40cb23cadd81509f26cec34be
Minkyu Park <minq.park@lge.com>
2026-06-01 15:08:15 -0700

    update env

--- thermo/1_prerequisite.sh
+++ thermo/1_prerequisite.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+sudo apt install python3-pip
+sudo pip install pw
 brew install python@3.14
 mkdir -p ~/.local/bin && cd ~/.local/bin
 unlink pip
 unlink pip3
 unlink python
 unlink python3
+set -x
 ln -s /home/linuxbrew/.linuxbrew/bin/pip3.14 pip
 ln -s /home/linuxbrew/.linuxbrew/bin/pip3.14 pip3
 ln -s /home/linuxbrew/.linuxbrew/bin/python3.14 python
 ln -s /home/linuxbrew/.linuxbrew/bin/python3.14 python3
-
+set +x
 #west
-pip3 install west
+sudo pip3 install west
 brew install cmake
 brew install ninja
 sudo apt-get install -y libssl-dev libdbus-1-dev libglib2.0-dev libavahi-client-dev libgirepository1.0-dev libcairo2-dev libreadline-dev
